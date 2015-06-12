f3c69b9f88d656617a572f23a1fbc270fb8db1fd
Charles d'Harcourt <noreply>
2015-06-12 06:05:52 +0000

    Add module alpha

---
+++ logic/alpha.js
@@ -0,0 +1,15 @@
+/**
+ * @module logic/alpha
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Alpha
+ * @extends Montage
+ */
+exports.Alpha = Montage.specialize(/** @lends Alpha# */ {
+    constructor: {
+        value: function Alpha() {
+            this.super();
+        }
+    }
+});
