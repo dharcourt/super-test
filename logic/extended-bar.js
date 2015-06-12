8717356e6a76a3befd2eeee326275dc731539144
Charles d'Harcourt <noreply>
2015-06-12 07:45:38 +0000

    Add module extended-bar

---
+++ logic/extended-bar.js
@@ -0,0 +1,15 @@
+/**
+ * @module logic/extended-bar
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class ExtendedBar
+ * @extends Montage
+ */
+exports.ExtendedBar = Montage.specialize(/** @lends ExtendedBar# */ {
+    constructor: {
+        value: function ExtendedBar() {
+            this.super();
+        }
+    }
+});
