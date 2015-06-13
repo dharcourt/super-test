67e5d01f1207742ad7be9f6d503b2dab815f9e17
Charles d'Harcourt <noreply>
2015-06-13 09:22:32 +0000

    Add module delta

---
+++ logic/delta.js
@@ -0,0 +1,15 @@
+/**
+ * @module logic/delta
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Delta
+ * @extends Montage
+ */
+exports.Delta = Montage.specialize(/** @lends Delta# */ {
+    constructor: {
+        value: function Delta() {
+            this.super();
+        }
+    }
+});
