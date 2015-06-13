ff650f5a6763216d967d5cdf5e8c3d9a79534d67
Charles d'Harcourt <noreply>
2015-06-13 09:22:16 +0000

    Add module bravo

---
+++ logic/bravo.js
@@ -0,0 +1,15 @@
+/**
+ * @module logic/bravo
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Bravo
+ * @extends Montage
+ */
+exports.Bravo = Montage.specialize(/** @lends Bravo# */ {
+    constructor: {
+        value: function Bravo() {
+            this.super();
+        }
+    }
+});
