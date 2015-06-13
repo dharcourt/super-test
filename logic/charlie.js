6f76bce2f08d795d3e9274538ef8075c7ec8ac2c
Charles d'Harcourt <noreply>
2015-06-13 09:22:24 +0000

    Add module charlie

---
+++ logic/charlie.js
@@ -0,0 +1,15 @@
+/**
+ * @module logic/charlie
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Charlie
+ * @extends Montage
+ */
+exports.Charlie = Montage.specialize(/** @lends Charlie# */ {
+    constructor: {
+        value: function Charlie() {
+            this.super();
+        }
+    }
+});
